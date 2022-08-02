1bf82d3972239687d5e2edb76b435efc7f5be901
Jim7rfp <Jim7fp@gmail.com>
2022-08-02 13:36:13 +0300

    Тема 30

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path, include
 from rest_framework import routers
-from api.views import CheckboxViewSet
+from api import views
+# from api.views import CheckboxViewSet
 
-router = routers.DefaultRouter()
-router.register('checkbox', CheckboxViewSet)
+# router = routers.DefaultRouter()
+# router.register('checkbox', CheckboxViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    # path('', include(router.urls)),
+    path('cbl/', views.checkbox_list, name='cbl'),
+    path('cbl/<int:pk>', views.checkbox_detail, name='cbd'),
+    path('cbcr/', views.checkbox_create, name='cbcr'),
+    path('cbup/<int:pk>', views.checkbox_update, name='cbup'),
+    path('cbdel/<int:pk>', views.checkbox_delete, name='cbdel'),
 ]
+
--- api/views.py
+++ api/views.py
@@ -1,12 +1,64 @@
 from django.shortcuts import render
-from rest_framework import viewsets
 from .models import Checkbox
-from api.serializers import CheckboxSerializer
+from .serializers import CheckboxSerializer
+from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
+# class CheckboxViewSet(viewsets.ModelViewSet):
+#     queryset = Checkbox.objects.all()
+#     serializer_class = CheckboxSerializer
 
-class CheckboxViewSet(viewsets.ModelViewSet):
-    queryset = Checkbox.objects.all()
-    serializer_class = CheckboxSerializer
+
+@api_view(['GET'])
+def checkbox_list(request):
+    checkbox = Checkbox.objects.all()
+    serializer = CheckboxSerializer(checkbox, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def checkbox_detail(request, pk):
+    try:
+        checkbox = Checkbox.objects.get(id=pk)
+        serializer = CheckboxSerializer(checkbox)
+    except Exception as err:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def checkbox_create(request):
+    serializer = CheckboxSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['PUT'])
+def checkbox_update(request, pk):
+    try:
+        checkbox = Checkbox.objects.get(id=pk)
+        serializer = CheckboxSerializer(instance=checkbox, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+    except Exception as err:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def checkbox_delete(request, pk):
+    # try:
+    #     checkbox = Checkbox.objects.get(id=pk)
+    #     serializer = CheckboxSerializer(checkbox)
+    # except Exception as err:
+    #     return Response(status=status.HTTP_404_NOT_FOUND)
+    # return Response(serializer.data)
+    checkbox = Checkbox.objects.get(id=pk)
+    checkbox.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
 
